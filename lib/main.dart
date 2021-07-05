59ce3693fe8df9c991ae6c71103043eec587a96f
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-07-06 05:29:17 +0530

    feat: generalized theme properties

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,39 @@
 import 'package:flutter/material.dart';
 
 import 'package:get/get.dart';
+import 'package:signin_firebase_getx/app/modules/home/bindings/home_binding.dart';
 
 import 'app/routes/app_pages.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding
+      .ensureInitialized(); //https://stackoverflow.com/questions/63873338/what-does-widgetsflutterbinding-ensureinitialized-do/63873689
+  // await Firebase.initializeApp();
+  HomeBinding().dependencies();
   runApp(
     GetMaterialApp(
-      title: "Application",
+      title: "Google Sign In Using GetX",
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      debugShowCheckedModeBanner: false,
+      themeMode: ThemeMode.dark,
+      darkTheme: ThemeData.dark().copyWith(
+        accentColor: Colors.yellow,
+        primaryColor: Color(0xFF141A31),
+        primaryColorDark: Color(0xFF081029),
+        scaffoldBackgroundColor: Color(0xFF141A31),
+        textSelectionTheme: TextSelectionThemeData(cursorColor: Colors.yellow),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ButtonStyle(
+              shape: MaterialStateProperty.all(
+                RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(20),
+                ),
+              ),
+              backgroundColor: MaterialStateProperty.all(Colors.yellowAccent),
+              padding: MaterialStateProperty.all(EdgeInsets.all(14))),
+        ),
+      ),
     ),
   );
 }
