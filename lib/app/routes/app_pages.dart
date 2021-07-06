27a8c9ea09348bb356b9e0fb71ed6ae31256049a
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-07-06 06:46:38 +0530

    feat: add login & welcome routes

--- lib/app/routes/app_pages.dart
+++ lib/app/routes/app_pages.dart
@@ -1,7 +1,11 @@
 import 'package:get/get.dart';
 
 import 'package:signin_firebase_getx/app/modules/home/bindings/home_binding.dart';
+import 'package:signin_firebase_getx/app/modules/home/bindings/login_binding.dart';
+import 'package:signin_firebase_getx/app/modules/home/bindings/welcome_binding.dart';
 import 'package:signin_firebase_getx/app/modules/home/views/home_view.dart';
+import 'package:signin_firebase_getx/app/modules/home/views/login_view.dart';
+import 'package:signin_firebase_getx/app/modules/home/views/welcome_view.dart';
 
 part 'app_routes.dart';
 
@@ -12,9 +16,25 @@ class AppPages {
 
   static final routes = [
     GetPage(
+      name: _Paths.LOGIN,
+      page: () => LoginView(),
+      binding: LoginBinding(),
+      transition: Transition.fadeIn,
+      transitionDuration: Duration(milliseconds: 500),
+    ),
+    GetPage(
       name: _Paths.HOME,
       page: () => HomeView(),
       binding: HomeBinding(),
+      transition: Transition.fadeIn,
+      transitionDuration: Duration(milliseconds: 500),
+    ),
+    GetPage(
+      name: _Paths.WELCOME,
+      page: () => WelcomeView(),
+      binding: WelcomeBinding(),
+      transition: Transition.fadeIn,
+      transitionDuration: Duration(milliseconds: 500),
     ),
   ];
 }
