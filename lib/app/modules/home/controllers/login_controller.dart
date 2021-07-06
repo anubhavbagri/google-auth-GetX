4ae906ddce0f1dbe1a5d01da09cf6cd182327506
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-07-06 06:48:23 +0530

    feat: add controllers

---
+++ lib/app/modules/home/controllers/login_controller.dart
@@ -0,0 +1,18 @@
+import 'package:get/get.dart';
+import 'package:signin_firebase_getx/app/modules/home/controllers/home_controller.dart';
+
+class LoginController extends GetxController {
+  HomeController homeController = Get.find<HomeController>();
+  @override
+  void onInit() async {
+    super.onInit();
+  }
+
+  @override
+  void onReady() {
+    super.onReady();
+  }
+
+  @override
+  void onClose() {}
+}
