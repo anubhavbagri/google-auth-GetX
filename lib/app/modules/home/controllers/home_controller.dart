8e334ac07e47fb6043657e9d6f2c3f8c7c7bdaa8
Anubhav Bagri <anubhav.199302029@muj.manipal.edu>
2021-07-06 06:47:50 +0530

    feat: add signIn logic for routing

--- lib/app/modules/home/controllers/home_controller.dart
+++ lib/app/modules/home/controllers/home_controller.dart
@@ -1,20 +1,41 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:signin_firebase_getx/app/routes/app_pages.dart';
 
 class HomeController extends GetxController {
-  //TODO: Implement HomeController
+  late GoogleSignIn googleSignIn;
+  var isSignIn = false.obs;
+  FirebaseAuth firebaseAuth = FirebaseAuth.instance;
 
-  final count = 0.obs;
   @override
   void onInit() {
     super.onInit();
   }
 
   @override
-  void onReady() {
+  void onReady() async {
+    googleSignIn = GoogleSignIn();
+    ever(isSignIn,
+        handleAuthStateChanged); //whenever value of isSignIn will change, at that time, the method handleAuthStateChanged will be called.
+    isSignIn.value = await firebaseAuth.currentUser !=
+        null; //Accordingly, isSignIn will contain true or false.
+    firebaseAuth.authStateChanges().listen((event) {
+      isSignIn.value = event != null;
+
+      ///event will contain the firebase user so accordingly, it'll check the condition and assign the value as true or false to isSignIn variable.
+    });
     super.onReady();
   }
 
   @override
   void onClose() {}
-  void increment() => count.value++;
+
+  void handleAuthStateChanged(isLoggedIn) {
+    if (isLoggedIn) {
+      Get.offAllNamed(Routes.WELCOME, arguments: firebaseAuth.currentUser);
+    } else {
+      Get.offAllNamed(Routes.LOGIN);
+    }
+  }
 }
